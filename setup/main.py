aa2889445e4ce85cffa7bbca7369cda1f4cf18db
Choe Hwanjin <choe.hwanjin@gmail.com>
2012-05-01 22:27:39 +0900

    Fix indentation
    ISSUE: https://github.com/choehwanjin/ibus-hangul/issues/8

--- setup/main.py
+++ setup/main.py
@@ -35,154 +35,154 @@ class Setup ():
     def __init__ (self, bus):
         self.__bus = bus
         self.__config = self.__bus.get_config()
-	self.__config.connect("value-changed", self.on_value_changed, None)
+        self.__config.connect("value-changed", self.on_value_changed, None)
 
-	ui_file = os.path.join(os.path.dirname(__file__), "setup.ui")
-	self.__builder = gtk.Builder()
-	self.__builder.set_translation_domain(config.gettext_package)
-	self.__builder.add_from_file(ui_file)
+        ui_file = os.path.join(os.path.dirname(__file__), "setup.ui")
+        self.__builder = gtk.Builder()
+        self.__builder.set_translation_domain(config.gettext_package)
+        self.__builder.add_from_file(ui_file)
 
-	# Hangul tab
-	pipe = subprocess.Popen([config.setuplibdir + '/hangul_keyboard_list'], stdout = subprocess.PIPE)
-	list = pipe.communicate()[0].split('\n')
-	
-	self.__hangul_keyboard = self.__builder.get_object("HangulKeyboard")
-	model = gtk.ListStore(str, str, int)
-	i = 0
-	for line in list:
-	    items = line.split('\t')
-	    if len(items) > 1:
-		model.append([items[1], items[0], i])
-		i+=1
+        # Hangul tab
+        pipe = subprocess.Popen([config.setuplibdir + '/hangul_keyboard_list'], stdout = subprocess.PIPE)
+        list = pipe.communicate()[0].split('\n')
 
-	self.__hangul_keyboard.set_model(model)
-	renderer = gtk.CellRendererText()
-	self.__hangul_keyboard.pack_start(renderer)
-	self.__hangul_keyboard.add_attribute(renderer, "text", 0)
+        self.__hangul_keyboard = self.__builder.get_object("HangulKeyboard")
+        model = gtk.ListStore(str, str, int)
+        i = 0
+        for line in list:
+            items = line.split('\t')
+            if len(items) > 1:
+                model.append([items[1], items[0], i])
+                i+=1
 
-	current = self.__read("HangulKeyboard", "2")
-	for i in model:
-	    if i[1] == current:
-		self.__hangul_keyboard.set_active(i[2])
-		break
+        self.__hangul_keyboard.set_model(model)
+        renderer = gtk.CellRendererText()
+        self.__hangul_keyboard.pack_start(renderer)
+        self.__hangul_keyboard.add_attribute(renderer, "text", 0)
 
-	self.__word_commit = self.__builder.get_object("WordCommit")
+        current = self.__read("HangulKeyboard", "2")
+        for i in model:
+            if i[1] == current:
+                self.__hangul_keyboard.set_active(i[2])
+                break
 
-	word_commit = self.__read("WordCommit", False)
+        self.__word_commit = self.__builder.get_object("WordCommit")
+
+        word_commit = self.__read("WordCommit", False)
         self.__word_commit.set_active(word_commit)
 
-	self.__auto_reorder = self.__builder.get_object("AutoReorder")
-	auto_reorder = self.__read("AutoReorder", True)
+        self.__auto_reorder = self.__builder.get_object("AutoReorder")
+        auto_reorder = self.__read("AutoReorder", True)
         self.__auto_reorder.set_active(auto_reorder)
 
-	# hanja tab
-	button = self.__builder.get_object("HanjaKeyListAddButton")
-	button.connect("clicked", self.on_hanja_key_add, None)
+        # hanja tab
+        button = self.__builder.get_object("HanjaKeyListAddButton")
+        button.connect("clicked", self.on_hanja_key_add, None)
 
-	button = self.__builder.get_object("HanjaKeyListRemoveButton")
-	button.connect("clicked", self.on_hanja_key_remove, None)
+        button = self.__builder.get_object("HanjaKeyListRemoveButton")
+        button.connect("clicked", self.on_hanja_key_remove, None)
 
-	model = gtk.ListStore(str)
+        model = gtk.ListStore(str)
 
-	keylist_str = self.__read("HanjaKeys", "Hangul_Hanja,F9")
-	self.__hanja_key_list_str = keylist_str.split(',')
-	for i in self.__hanja_key_list_str:
-	    model.append([i])
+        keylist_str = self.__read("HanjaKeys", "Hangul_Hanja,F9")
+        self.__hanja_key_list_str = keylist_str.split(',')
+        for i in self.__hanja_key_list_str:
+            model.append([i])
 
-	self.__hanja_key_list = self.__builder.get_object("HanjaKeyList")
-	self.__hanja_key_list.set_model(model)
-	column = gtk.TreeViewColumn()
-	column.set_title("key")
-	renderer = gtk.CellRendererText()
-	column.pack_start(renderer)
-	column.add_attribute(renderer, "text", 0)
-	self.__hanja_key_list.append_column(column)
+        self.__hanja_key_list = self.__builder.get_object("HanjaKeyList")
+        self.__hanja_key_list.set_model(model)
+        column = gtk.TreeViewColumn()
+        column.set_title("key")
+        renderer = gtk.CellRendererText()
+        column.pack_start(renderer)
+        column.add_attribute(renderer, "text", 0)
+        self.__hanja_key_list.append_column(column)
 
 
-	# advanced tab
-	notebook = self.__builder.get_object("SetupNotebook")
-	notebook.remove_page(2)
+        # advanced tab
+        notebook = self.__builder.get_object("SetupNotebook")
+        notebook.remove_page(2)
 
-	# setup dialog
-	self.__window = self.__builder.get_object("SetupDialog")
-	icon_file = os.path.join(config.pkgdatadir, "icons", "ibus-hangul.svg")
-	self.__window.set_icon_from_file(icon_file)
-	self.__window.connect("response", self.on_response, None)
-	self.__window.show()
+        # setup dialog
+        self.__window = self.__builder.get_object("SetupDialog")
+        icon_file = os.path.join(config.pkgdatadir, "icons", "ibus-hangul.svg")
+        self.__window.set_icon_from_file(icon_file)
+        self.__window.connect("response", self.on_response, None)
+        self.__window.show()
 
-	ok_button = self.__builder.get_object("button_cancel")
-	ok_button.grab_focus()
+        ok_button = self.__builder.get_object("button_cancel")
+        ok_button.grab_focus()
 
     def run(self):
-	res = self.__window.run()
-	if (res == gtk.RESPONSE_OK):
-	    self.on_ok()
-	self.__window.destroy()
+        res = self.__window.run()
+        if (res == gtk.RESPONSE_OK):
+            self.on_ok()
+        self.__window.destroy()
 
     def apply(self):
-	model = self.__hangul_keyboard.get_model()
-	i = self.__hangul_keyboard.get_active()
-	self.__write("HangulKeyboard", model[i][1])
+        model = self.__hangul_keyboard.get_model()
+        i = self.__hangul_keyboard.get_active()
+        self.__write("HangulKeyboard", model[i][1])
 
         word_commit = self.__word_commit.get_active()
         self.__write("WordCommit", word_commit)
 
-	model = self.__hanja_key_list.get_model()
-	str = ""
-	iter = model.get_iter_first()
-	while iter:
-	    if len(str) > 0:
-		str += ","
-		str += model.get_value(iter, 0)
-	    else:
-		str += model.get_value(iter, 0)
-	    iter = model.iter_next(iter)
-	self.__write("HanjaKeys", str)
+        model = self.__hanja_key_list.get_model()
+        str = ""
+        iter = model.get_iter_first()
+        while iter:
+            if len(str) > 0:
+                str += ","
+                str += model.get_value(iter, 0)
+            else:
+                str += model.get_value(iter, 0)
+            iter = model.iter_next(iter)
+        self.__write("HanjaKeys", str)
 
     def on_response(self, widget, id, data = None):
-	if id == gtk.RESPONSE_APPLY:
-	    self.apply()
-	    widget.emit_stop_by_name("response")
-	if id == gtk.RESPONSE_NONE:
-	    widget.emit_stop_by_name("response")
+        if id == gtk.RESPONSE_APPLY:
+            self.apply()
+            widget.emit_stop_by_name("response")
+        if id == gtk.RESPONSE_NONE:
+            widget.emit_stop_by_name("response")
 
     def on_ok(self):
-	self.apply()
+        self.apply()
 
     def on_hanja_key_add(self, widget, data = None):
-	dialog = KeyCaptureDialog(_("Select Hanja key"), self.__window)
-	res = dialog.run()
-	if res == gtk.RESPONSE_OK:
-	    key_str = dialog.get_key_string()
-	    if len(key_str) > 0:
-		model = self.__hanja_key_list.get_model()
-		iter = model.get_iter_first()
-		while iter:
-		    str = model.get_value(iter, 0)
-		    if str == key_str:
-			model.remove(iter)
-			break
-		    iter = model.iter_next(iter)
+        dialog = KeyCaptureDialog(_("Select Hanja key"), self.__window)
+        res = dialog.run()
+        if res == gtk.RESPONSE_OK:
+            key_str = dialog.get_key_string()
+            if len(key_str) > 0:
+                model = self.__hanja_key_list.get_model()
+                iter = model.get_iter_first()
+                while iter:
+                    str = model.get_value(iter, 0)
+                    if str == key_str:
+                        model.remove(iter)
+                        break
+                    iter = model.iter_next(iter)
 
-		model.append([key_str])
-	dialog.destroy()
+                model.append([key_str])
+        dialog.destroy()
 
     def on_hanja_key_remove(self, widget, data = None):
-	selection = self.__hanja_key_list.get_selection()
-	(model, iter) = selection.get_selected()
-	if model and iter:
-	    model.remove(iter)
+        selection = self.__hanja_key_list.get_selection()
+        (model, iter) = selection.get_selected()
+        if model and iter:
+            model.remove(iter)
 
     def on_value_changed(self, config, section, name, value, data):
-	if section == "engine/Hangul":
-	    if name == "HangulKeyboard":
-		model = self.__hangul_keyboard.get_model()
-		for i in model:
-		    if i[1] == value:
-			self.__hangul_keyboard.set_active(i[2])
-			break
-	    elif name == "HanjaKeys":
-		self.__hanja_key_list_str = value.split(',')
+        if section == "engine/Hangul":
+            if name == "HangulKeyboard":
+                model = self.__hangul_keyboard.get_model()
+                for i in model:
+                    if i[1] == value:
+                        self.__hangul_keyboard.set_active(i[2])
+                        break
+            elif name == "HanjaKeys":
+                self.__hanja_key_list_str = value.split(',')
 
     def __read(self, name, v):
         return self.__config.get_value("engine/Hangul", name, v)
